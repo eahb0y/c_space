069f3aea6feb745196bc2da25ba63e3d219903f3
eahb0y <iskandarmister323@gamil.com>
2023-07-14 12:28:00 +0500

    testing time

--- lib/feature/client/presintation/bloc/time_bloc/time_bloc.dart
+++ lib/feature/client/presintation/bloc/time_bloc/time_bloc.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:c_space/feature/client/data/model/client_get_time_model.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 
 part 'time_event.dart';
 
@@ -14,50 +13,14 @@ class TimeBloc extends Bloc<TimeEvent, TimeState> {
   late int clientTotalTime;
 
   TimeBloc() : super(TimeState(clientTotalTime: '5000')) {
-    on<GetTotalTime>(_convertTime);
+    on<GetTotalTime>(_calculateTotalTime);
   }
-  String convertTo24HourFormat({required String checkIn, required String checkOut}) {
-    List<String> timeParts = "$checkIn $checkOut".split("  ");
-    String timeString = timeParts[0];
-    String meridiem = timeParts.length > 1 ? timeParts[1].toUpperCase() : '';
-    print(timeString);
-    print('afafasasfafsfsdf');
-    List<String> timeDigits = timeString.split(':');
-    int hours = int.parse(timeDigits[0]);
-    int minutes = int.parse(timeDigits[1]);
-    int seconds = timeDigits.length > 2 ? int.parse(timeDigits[2]) : 0;
-    print(hours);
-    print('asdasdasfasfafasfasfasfasf');
-    if (meridiem == 'PM' && hours < 12) {
-      hours += 12;
-    } else if (meridiem == 'AM' && hours == 12) {
-      hours = 0;
+  Future<void> _calculateTotalTime(GetTotalTime event, Emitter<TimeState> emit) async {
+    List<String> times = [];
+    for(var time in event.clientDate){
+      times.addAll([time.checkIn, time.checkOut]);
     }
-    String formattedTime =
-        '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}';
-    print(formattedTime);
-    return formattedTime;
-  }
-
-  Future<void> _convertTime(GetTotalTime event, Emitter<TimeState> emit) async {
-    if (event.clientDate.length > 0) {
-      for (var element in event.clientDate) {
-        if (element != '--/--') {
-          String converting = convertTo24HourFormat(
-            checkIn: element.checkIn,
-            checkOut: element.checkOut,
-          );
-          List<String> parts = converting.split(':');
-          int hours = int.parse(parts[0]);
-          int minutes = int.parse(parts[1]);
-          totalMinutes += hours * 60 + minutes;
-        }
-      }
-      clientTotalTime = 6000 - totalMinutes;
-      emit(state.copyWith(clientTotalTime: clientTotalTime.toString()));
-    } else {
-      throw 'errorrrrrr';
-    }
+    print(times);
   }
 
 
